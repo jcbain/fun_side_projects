68138ccd1f51acb717bceac2517919adf2c4ae1c
James Bain <jamescbain@gmail.com>
2017-09-22 08:52:28 -0500

    document unique_digits

--- unique_digits/unique_digits.py
+++ unique_digits/unique_digits.py
@@ -1,14 +1,59 @@
 from collections import Counter
 
 def separate_digits(val):
+    """
+    separate the digits of a number in a list of its digits
+
+    Parameters
+    -----
+    val : int
+      number to separate out
+
+    Returns
+    -----
+    list
+      a list of ints of the digits that make up val
+    """
     return [int(x) for x in str(val)]
 
 def any_intersection(x,y):
+    """
+    check to see if there are any intersecting values between two lists
+
+    Parameters
+    -----
+    x : list
+      first list of values
+
+    y : list
+      second list of values
+
+    Returns
+    -----
+    bool
+      True if there is an intersection, False otherwise
+    """
     inter = set(x).intersection(set(y))
     return bool(inter)
 
 def run_exponential(exp,upper_bound = 10000):
+    """
+    finds the values between 0 and an upper bound that contains all unique digits
+    and shares no digits with its value raised to some exponent
 
+    Parameters
+    -----
+    exp : int
+      exponent to raise the values to
+
+    upper_bound : int
+      the upper limit of where the list should stop at
+
+    Returns
+    lists
+      first list is a list the base values that meet the criteria
+      second list is a list of the base values that work raised to the exponent 
+    """
     candidates_result = []
     candidates_base = []
     for i in range(0,upper_bound):
