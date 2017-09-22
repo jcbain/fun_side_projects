a52d99d3d0959557b5f4597a6e6fb86500dacb70
James Bain <jamescbain@gmail.com>
2017-09-22 08:38:05 -0500

    work out issues with finding unique digits

--- unique_digits/unique_digits.py
+++ unique_digits/unique_digits.py
@@ -1,4 +1,5 @@
 from collections import Counter
+
 def separate_digits(val):
     return [int(x) for x in str(val)]
 
@@ -7,18 +8,26 @@ def any_intersection(x,y):
     return bool(inter)
 
 def run_exponential(exp,upper_bound = 10000):
-    
-    candidates = []   
+
+    candidates_result = []
+    candidates_base = []
     for i in range(0,upper_bound):
         num = i ** exp
-        
-        compound = str(num) + str(i)
+
+        compound = str(i) + str(exp)
         separate_compound = separate_digits(compound)
         separate_num = separate_digits(num)
-      
-        if (not any_intersection(separate_compound,separate_num)) & (all(list(Counter(separate_digits(i)).values())) <= 1):
-           candidates.append(num)
 
+        base_digit_counts = list(Counter(separate_digits(i)).values())
 
-    return max(candidates)
- 
+        intersection_check = not any_intersection(separate_num,separate_compound)
+        base_check = not any(i > 1 for i in base_digit_counts)
+
+        if intersection_check & base_check:
+           candidates_result.append(num)
+           candidates_base.append(i)
+
+    return candidates_base,candidates_result
+
+if __name__ == '__main__':
+    print(run_exponential(6, 1000000000))
